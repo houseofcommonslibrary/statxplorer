96027afe2fd4b667f9c712d7ab1b1c17e73d2573
Oliver Hawkins <oli@olihawkins.com>
2019-03-14 14:52:42 +0000

    0.2.4 - Added has_api_key

--- R/cache.R
+++ R/cache.R
@@ -23,24 +23,7 @@ set_api_key <- function(api_key) {
     assign(CACHE_API_KEY, api_key, envir = cache)
 }
 
-#' Get the api key
-#'
-#' \code{get_api_key} gets the api key that the package uses for communciation
-#' with the server from the package cache.
-#'
-#' @return The api key as a string
-#' @export
-
-get_api_key <- function() {
-    if (! exists(CACHE_API_KEY, envir = cache)) {
-        stop(stringr::str_c(
-            "No API key provided: use statxplorer::set_api_key() or ",
-            "statxplorer::load_api_key() to provide your key"))
-    }
-    get(CACHE_API_KEY, envir = cache)
-}
-
-#' Loads the api key from a file
+#' Load the api key from a file
 #'
 #' \code{load_api_key} is a convenience method that loads the api key from the
 #' given file and sets it for the session.
@@ -52,4 +35,33 @@ get_api_key <- function() {
 load_api_key <- function(filename) {
     api_key = stringr::str_trim(readr::read_file(filename))
     set_api_key(api_key)
+}
+
+#' Get the api key
+#'
+#' \code{get_api_key} gets the api key that the package uses for communciation
+#' with the server from the package cache.
+#'
+#' @return The api key as a string
+#' @keywords internal
+
+get_api_key <- function() {
+    if (! exists(CACHE_API_KEY, envir = cache)) {
+        stop(stringr::str_c(
+            "No API key provided: use statxplorer::set_api_key() or ",
+            "statxplorer::load_api_key() to provide your key"))
+    }
+    get(CACHE_API_KEY, envir = cache)
+}
+
+#' Check an api key has been set
+#'
+#' \code{has_api_key} checks that an api key for communicating with the server
+#' has been set in the package cache.
+#'
+#' @return A boolean indicating whether the cache contains an api_key
+#' @export
+
+has_api_key <- function() {
+    exists(CACHE_API_KEY, envir = cache))
 }
